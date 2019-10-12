f09bbc1422ad3a436c54cd78394935f2e6f74c19
Meisuleen Kased <meisuleen@gmail.com>
2019-10-12 17:29:43 +0000

    Automatically backed up by Learn

--- db/migrate/20191011194517_create_doctors.rb
+++ db/migrate/20191011194517_create_doctors.rb
@@ -2,17 +2,8 @@ class CreateDoctors < ActiveRecord::Migration[5.0]
   def change
     create_table :doctors do |t|
       t.string :name
-      t.integer :department
+      t.string :department
 
       t.timestamps
     end
-  end
-
-  def up
-     change_column :CreateDoctors, :department, :string
-  end
-
-  def down
-     change_column :CreateDoctors, :department, :integer
-  end
 end
